942f7dc12dc6f56bd7477970a2311f7c4198c702
IvanPiankov <vanypyankov@gmail.com>
2022-09-04 12:21:22 +0300

    Added models

--- tictactoe/models.py
+++ tictactoe/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nickname = models.CharField(max_length=35)
+
+
+class Match(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    players = models.ManyToManyField(Player)
+    started = models.BooleanField(default=False)
+
